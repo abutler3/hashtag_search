c6212acc139bf0806ff93e186ca1c70162ecfcfe
Drew Butler <abutler3@gmail.com>
2013-03-08 13:45:06 -0600

    Minor refactor of the specs

--- spec/integration/search_twitter_spec.rb
+++ spec/integration/search_twitter_spec.rb
@@ -3,11 +3,7 @@ require 'spec_helper'
 feature 'Search Twitter for a term' do
   scenario 'searching by a hashtag' do
     search_for 'Rails'
-    expect(page).to have_css 'li.tweet', count: 15
-
-	 all('li.tweet').each do |tweet|
-	 	expect(tweet.text).to match /#rails/i
-	 end
+    user_sees_tweets 15, '#Rails'
   end
 
   scenario 'searching with known results' do
@@ -15,12 +11,20 @@ feature 'Search Twitter for a term' do
     FakeTwitter['#Rails'] = 3.times.map { { text: 'Testing is awesome' } }
 
     search_for 'Rails'
-    expect(page).to have_css 'li.tweet', count: 3, text: 'Testing is awesome'
+    user_sees_tweets 3, 'Testing is awesome'
   end
 
   def search_for(term)
     visit root_path
     fill_in 'Search', with: term
     click_button 'Search'
+  end
+
+  def user_sees_tweets(count, text)
+   expect(page).to have_css 'li.tweet', count: count
+
+   all('li.tweet').each do |tweet|
+    expect(tweet.text).to match /#{text}/i
+   end
   end
 end
